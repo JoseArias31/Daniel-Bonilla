eea73dad63bcdf85aa5f33df9127e0fe3b31fd69
Jose Arias <115059715+Monoarias777@users.noreply.github.com>
2023-07-03 11:57:46 -0400

    Backend Project Daniel

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import doubleNumber from './double.js'
+
+
+
+console.log("Hello World, this is Daniel.")
+
+console.log("\n---------");
+
+console.log(doubleNumber(4));
+
+console.log("\n---------");
