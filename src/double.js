cbd7dcef53a167d0d66f321f516d16ab44206fd3
Jose Arias <115059715+Monoarias777@users.noreply.github.com>
2023-07-03 13:04:27 -0400

    phase 2

---
+++ src/double.js
@@ -0,0 +1,8 @@
+function doubleNumber(num){
+
+    return num * 2;
+    
+    }
+    
+    export default doubleNumber;
+    
--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
+import doubleNumber from './double.js'
+import {response1, response2, response3, response4, response5, response6} from './responseToQuote.js'
+
 
 console.log("Hello World, this is Daniel.")
 
 console.log("\n---------");
 
+console.log(doubleNumber(4));
+
+console.log("\n---------");
+
+console.log(response1);
+console.log(response2);
+console.log(response3);
+console.log(response4);
+console.log(response5);
+console.log(response6);
+
+console.log("\n---------");
